03a9fb66558e99febde2005ffbe043c863c6100f
Federico Moretti <hello@federicomoretti.it>
2024-06-19 20:10:19 +0200

    feat: refactoring the folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
+import { CreateFolder } from './lib/utils';
 
 /**
  * 
@@ -10,28 +10,15 @@ const init = async () => {
 };
 
 /**
- * Create a new folder of the given name (if available) or fallback to the default name.
- * @returns {string}
- */
-const createFolder: any = (): string => {
-  if (process.argv.length > 2) {
-    const folderName: string = process.argv[2];
-    fs.mkdirSync(folderName);
-    return folderName;
-  } else {
-    const defaultFolderName: string = 'my-lambda';
-    fs.mkdirSync(defaultFolderName);
-    return defaultFolderName;
-  }
-};
-
-/**
  * 
  */
 const run: any = async () => {
   init();
+
   // Create a new folder.
-  const folderName: string = createFolder();
+  const folderName: string = CreateFolder();
+
+  //
 
 };
 
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,1 @@
+export { CreateFolder } from './shell';

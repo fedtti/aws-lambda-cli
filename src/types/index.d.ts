9b13203e4850fe33bdb6158d96d2e7190455db15
Federico Moretti <hello@federicomoretti.it>
2024-07-12 18:46:50 +0200

    fix: update documentation

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,6 @@ interface NpmOptions {
   initAuthorName?: string;
   initAuthorUrl?: url;
   initLicense?: string;
-  initVersion?: string;
+  initVersion?: string; // Use the SemVer standard.
   initScope?: string;
 }

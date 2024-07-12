4d3115c0d9fb33c1b6bea9f062999647ae2ef484
Federico Moretti <hello@federicomoretti.it>
2024-07-12 12:33:05 +0200

    fix: update documentation

--- src/lib/utils/shell.ts
+++ src/lib/utils/shell.ts
@@ -1,5 +1,9 @@
 import { exec, spawn } from 'child_process';
 
+/**
+ * Initialize a new Git repository in the specified folder.
+ * @param {string} folder - 
+ */
 export const GitInit: any = (folder: string): any => {
   const commands: string[] = [`cd ${folder}`, 'git init -q'];
 };

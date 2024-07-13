28d6c07e790ebebf27772cd209ff100c70904d4f
Federico Moretti <hello@federicomoretti.it>
2024-07-13 10:02:26 +0200

    feat: check if the folder already exists

--- src/lib/utils/file-manager.ts
+++ src/lib/utils/file-manager.ts
@@ -1,18 +1,13 @@
 import fs from 'fs';
 
 /**
- * Create a new empty folder of the given name, if available, or fallback to the default one.
+ * Create a new empty folder of the given name, if available, or fallback to a default one.
  * @returns {string}
  */
 export const CreateFolder: any = async (): Promise<string> => {
-  if (process.argv.length >= 2) { // TODO: @fedtti - Exclude `--help`, `-h`, `help`, and other possible argument values.
-    const folderName: string = process.argv[2];
-    fs.mkdirSync(folderName);
+    const folderName: string = process.argv[2] || 'my-lambda';
+    if (!fs.existsSync(`./${folderName}`)){
+      fs.mkdirSync(folderName);
+    }
     return folderName;
-  } else {
-    const defaultFolderName: string = 'my-lambda';
-    fs.mkdirSync(defaultFolderName);
-    return defaultFolderName;
-  }
 };
-

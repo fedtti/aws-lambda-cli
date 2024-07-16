1d6dd63bdc79357bb656825e085cd7ec18dfdd6c
Federico Moretti <hello@federicomoretti.it>
2024-07-16 10:57:28 +0200

    fix: update Serverless configuration file to provide TypeScript (default, optional) support

--- src/lib/utils/file-manager.ts
+++ src/lib/utils/file-manager.ts
@@ -21,7 +21,8 @@ export const CreateSlsConfigFile: any = (folder: string, answers: UserAnswers): 
   let data: string = '# serverless.yml\n\n';
   const service: string = `service: ${answers.packageName}\n\nprovider:\n  name: aws\n  runtime: nodejs20.x\n  stage: dev\n  region: ${process.env.AWS_DEFAULT_REGION || process.env.AWS_REGION || 'eu-south-1'}\n  timeout: 30`;
   const functions: string = `functions:\n  ${answers.packageName}:\n  name:  ${answers.packageName}-\${self:provider.stage}\n  description: ${answers.packageDescription}\n  handler: ./dist/index.handler\n  timeout: 30\n  environment:\n    NODE_ENV: ${process.env.NODE_ENV || 'development'}\n    DEBUG: '*,-body-parser:*,-express:*,-*:trace'\n  events:\n    - http: 'ANY /'\n    - http: 'ANY /{proxy+}'`;
-  const packageSection: string = `package:\n  individually: true\n  patterns:\n    - dist/**\n    - '!src/**'\n    - '!.git/**'\n    - '!.DS_Store'\n    - '!.env'\n    - '!.gitignore'\n    - '!.npmrc'\n    - '!.nvmrc'\n    - '!.vscode'\n    - '!README.md'\n    - '!tsconfig.json'`; // Set to `packageSection` because `package` is a reserved word.
+  let packageSection: string = `package:\n  individually: true\n  patterns:\n    - dist/**\n    - '!src/**'\n    - '!.git/**'\n    - '!.DS_Store'\n    - '!.env'\n    - '!.gitignore'\n    - '!.npmrc'\n    - '!.nvmrc'\n    - '!.vscode'\n    - '!README.md'`; // Set to `packageSection` because `package` is a reserved word.
+  answers.typeScriptSupport ? packageSection += `\n    - '!tsconfig.json'` : 0;
   data += `${service}\n\n${functions}\n\n${packageSection}\n\n`;
   data += `configValidationMode: error\n`;
   fs.writeFileSync(`./${folder}/serverless.yml`, data);

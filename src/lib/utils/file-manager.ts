56283db99e86ca2cc490e18646fa0f2cc1929237
Federico Moretti <hello@federicomoretti.it>
2024-07-29 20:10:18 +0200

    fix: update Serverless handler creation

--- src/lib/utils/file-manager.ts
+++ src/lib/utils/file-manager.ts
@@ -36,8 +36,7 @@ export const CreateSlsConfigFile: any = (folder: string, answers: UserAnswers): 
   const functions: string = `functions:\n  ${answers.packageName}:\n  name:  ${answers.packageName}-\${self:provider.stage}\n  description: ${answers.packageDescription}\n  handler: ./dist/index.handler\n  timeout: 30\n  environment:\n    NODE_ENV: ${process.env.NODE_ENV || 'development'}\n    DEBUG: '*,-body-parser:*,-express:*,-*:trace'\n  events:\n    - http: 'ANY /'\n    - http: 'ANY /{proxy+}'`;
   let packageSection: string = `package:\n  individually: true\n  patterns:\n    - dist/**\n    - '!src/**'\n    - '!.git/**'\n    - '!.DS_Store'\n    - '!.env'\n    - '!.gitignore'\n    - '!.npmrc'\n    - '!.nvmrc'\n    - '!.vscode'\n    - '!README.md'`; // Set to `packageSection` because `package` is a reserved word.
   !!answers.typeScriptSupport ? packageSection += `\n    - '!tsconfig.json'` : 0;
-  data += `${service}\n\n${functions}\n\n${packageSection}\n\n`;
-  data += `configValidationMode: error\n`;
+  data += `${service}\n\n${functions}\n\n${packageSection}\n\nconfigValidationMode: error\n`;
   fs.writeFileSync(`./${folder}/serverless.yml`, data);
 };
 
@@ -48,9 +47,8 @@ export const CreateSlsConfigFile: any = (folder: string, answers: UserAnswers): 
  */
 export const CreateAwsLambdaHandlerFile: any = (folder: string, answers: UserAnswers): any => {
   let data: string = '#!/usr/bin/env node\n\n';
-
+  // TODO: @fedtti - Add configuration here.
   const extension: string = !!answers.typeScriptSupport ? 'ts' : 'js';
-
   if (!fs.existsSync(`./${folder}/src`)){
     fs.mkdirSync(`${folder}/src`);
   }
